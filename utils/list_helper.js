ab5cf953dc941df974b539283f88d3a7f629c8ba
aarnif <aarni.fagerstrom@gmail.com>
2023-10-16 10:54:20 +0300

    Tehtava 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const sumOfLikes = (sum, blog) => sum + blog.likes;
+  return blogs.length === 0 ? 0 : blogs.reduce(sumOfLikes, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
