1fbee9b10de875ea227658b2a0ddf9694368f74b
aarnif <aarni.fagerstrom@gmail.com>
2023-10-17 10:46:46 +0300

    Refaktoroi koodia

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -15,64 +15,70 @@ beforeEach(async () => {
   }
 });
 
-test("blogs are json objects", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("view all blogs", () => {
+  test("blogs are json objects", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs");
+    expect(response.body).toHaveLength(blogs.length);
+  });
 });
 
-test("all blogs are returned", async () => {
-  const response = await api.get("/api/blogs");
-  expect(response.body).toHaveLength(blogs.length);
+describe("view single blog", () => {
+  test("blogs identification field is named 'id'", async () => {
+    const response = await api.get("/api/blogs");
+    expect(response.body[0]?.id).not.toBe(undefined);
+  });
 });
 
-test("blogs identification field is named 'id'", async () => {
-  const response = await api.get("/api/blogs");
-  expect(response.body[0]?.id).not.toBe(undefined);
-});
+describe("add a single blog", () => {
+  test("add new blog to database", async () => {
+    const newBlogContent = {
+      title: "Test Blog",
+      author: "John Doe",
+      url: "http://www.fakeblogaddress.com/post100",
+      likes: 20,
+    };
+    const allPosts = await api.get("/api/blogs");
+    await api.post("/api/blogs").send(newBlogContent);
+    const updatedPosts = await api.get("/api/blogs");
+    expect(updatedPosts.body).toHaveLength(allPosts.body.length + 1);
+  });
 
-test("Add new blog to database", async () => {
-  const newBlogContent = {
-    title: "Test Blog",
-    author: "John Doe",
-    url: "http://www.fakeblogaddress.com/post100",
-    likes: 20,
-  };
-  const allPosts = await api.get("/api/blogs");
-  await api.post("/api/blogs").send(newBlogContent);
-  const updatedPosts = await api.get("/api/blogs");
-  expect(updatedPosts.body).toHaveLength(allPosts.body.length + 1);
-});
+  test("add new blog without likes", async () => {
+    const newBlogContent = {
+      title: "Test Blog",
+      author: "Jane Doe",
+      url: "http://www.fakeblogaddress.com/post101",
+    };
+    await api.post("/api/blogs").send(newBlogContent);
+    const updatedPosts = await api.get("/api/blogs");
+    const findNewPost = updatedPosts.body.find(
+      (post) => post.title === newBlogContent.title
+    );
+    expect(findNewPost.likes).toBe(0);
+  });
 
-test("Add new blog without likes", async () => {
-  const newBlogContent = {
-    title: "Test Blog",
-    author: "Jane Doe",
-    url: "http://www.fakeblogaddress.com/post101",
-  };
-  await api.post("/api/blogs").send(newBlogContent);
-  const updatedPosts = await api.get("/api/blogs");
-  const findNewPost = updatedPosts.body.find(
-    (post) => post.title === newBlogContent.title
-  );
-  expect(findNewPost.likes).toBe(0);
-});
+  test("try to add new blog without title", async () => {
+    const newBlogContentNoTitle = {
+      author: "Jane Doe",
+      url: "http://www.fakeblogaddress.com/post101",
+    };
+    await api.post("/api/blogs").send(newBlogContentNoTitle).expect(400);
+  });
 
-test("Try to add new blog without title", async () => {
-  const newBlogContentNoTitle = {
-    author: "Jane Doe",
-    url: "http://www.fakeblogaddress.com/post101",
-  };
-  await api.post("/api/blogs").send(newBlogContentNoTitle).expect(400);
-});
-
-test("Try to add new blog without url", async () => {
-  const newBlogContentNoURL = {
-    title: "Test Blog",
-    author: "Jane Doe",
-  };
-  await api.post("/api/blogs").send(newBlogContentNoURL).expect(400);
+  test("try to add new blog without url", async () => {
+    const newBlogContentNoURL = {
+      title: "Test Blog",
+      author: "Jane Doe",
+    };
+    await api.post("/api/blogs").send(newBlogContentNoURL).expect(400);
+  });
 });
 
 afterAll(async () => {
